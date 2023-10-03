5d6270697fec3c02aff6f889feddef69b6b51c8e
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-10-04 01:16:01 +0900

    add udp/sender.cpp

---
+++ udp/sender.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+
+using boost::asio::ip::udp;
+
+int main(int argc, char* argv[]){
+    try {
+        boost::asio::io_context io_context;
+
+        udp::socket socket(io_context, udp::endpoint(udp::v4(), 0));
+        udp::endpoint receiver_endpoint(boost::asio::ip::make_address("172.17.0.3/16"), 12345);
+
+        std::string message = "hello world";
+        socket.send_to(boost::asio::buffer(message), receiver_endpoint);
+
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+
+    return 0;
+}
+
